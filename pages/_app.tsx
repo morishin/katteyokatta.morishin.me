2fbfcdf2ed27ae5768b16d4dcfecfc0be49d2af4
morishin <sntr92@gmail.com>
2022-08-31 00:48:35 +0900

    Introduce ChakraUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ChakraProvider>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ChakraProvider>
   );
 }
 
